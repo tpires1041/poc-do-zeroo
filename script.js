5b85018ae0ba4d9d1b61fe197348afd4ba9b601b
tpires1041 <tiagopires2501@outlook.com>
2024-05-31 13:31:56 -0400

    Função echo
    adicionada a function echo

--- script.js
+++ script.js
@@ -2,15 +2,15 @@ const axios = require('axios');
 const RiveScript = require('rivescript');
 
 
-const string1 = "Quem deve aguardar para doar sangue?"
-const string2 = "Quais vacinas precisam aguardar?"
-const string3 = "Quem não pode doar sangue?"
-const string4 = "Quais são as recomendações para quem irá doar?"
-const string5 = "Qual o intervalo entre doações?"
-const string6 = "O que preciso para doar sangue?"
+const string1 = "quem deve aguardar para doar sangue?"
+const string2 = "quais vacinas precisam aguardar?"
+const string3 = "quem não pode doar sangue?"
+const string4 = "quais são as recomendações para quem irá doar?"
+const string5 = "qual o intervalo entre doações?"
+const string6 = "o que preciso para doar sangue?"
 
 
-const brains = 'https://gist.githubusercontent.com/tpires1041/da6a583aeb2d840193b091b81ed77051/raw/a81fa4cf531b80489f1667fd07b5658274960dca/gistfile1.txt';
+const brains = 'https://gist.githubusercontent.com/tpires1041/da6a583aeb2d840193b091b81ed77051/raw/4604a8d30f5e414d7ee030cb47257a34889e7eb2/gistfile1.txt';
 let bot = new RiveScript();
 
 axios.get(brains).then(response => {
@@ -28,17 +28,9 @@ const token = '6972642226:AAEoBLk1WeoLB3mgyYA9QvEpFQG4XY89qzM';
 
 const bott = new TelegramBot(token, { polling: true });
 
-
 bott.onText(/\/echo (.+)/, (msg, match) => {
     const chatId = msg.chat.id;
-    const resp = match[1]; 
-    bott.sendMessage(chatId, resp); 
+    const resp = match[1];
+    bott.sendMessage(chatId, resp);
 });
 
-
-bott.on('message', (msg) => {
-   const chatId = msg.chat.id;
-   bot.reply("local-user", msg.text).then(function(reply) {
-       bott.sendMessage(chatId, botReply(reply));
-   });
-});
