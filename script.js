56fb07a5d690e110c8237f7707630ccee63d6a46
tpires1041 <tiagopires2501@outlook.com>
2024-05-31 13:21:16 -0400

    Strings
    As strings que representam o input ideal foram adicionadas.

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
 const axios = require('axios');
 const RiveScript = require('rivescript');
 
-let bot = new RiveScript();
+
+const string1 = "Quem deve aguardar para doar sangue?"
+const string2 = "Quais vacinas precisam aguardar?"
+const string3 = "Quem não pode doar sangue?"
+const string4 = "Quais são as recomendações para quem irá doar?"
+const string5 = "Qual o intervalo entre doações?"
+const string6 = "O que preciso para doar sangue?"
+
 
 const brains = 'https://gist.githubusercontent.com/tpires1041/da6a583aeb2d840193b091b81ed77051/raw/a81fa4cf531b80489f1667fd07b5658274960dca/gistfile1.txt';
+let bot = new RiveScript();
 
 axios.get(brains).then(response => {
     bot.stream(response.data);
@@ -14,20 +22,20 @@ axios.get(brains).then(response => {
 
 const TelegramBot = require('node-telegram-bot-api');
 
-// Substitua 'YOUR_TELEGRAM_BOT_TOKEN' pelo token do seu bot
+
 const token = '6972642226:AAEoBLk1WeoLB3mgyYA9QvEpFQG4XY89qzM';
 
-// Crie um bot que usa 'polling' para buscar novas atualizações
+
 const bott = new TelegramBot(token, { polling: true });
 
-// Responde ao comando "/echo [qualquer coisa]"
+
 bott.onText(/\/echo (.+)/, (msg, match) => {
     const chatId = msg.chat.id;
-    const resp = match[1]; // Captura o texto após o comando "/echo"
-    bott.sendMessage(chatId, resp); // Envia a resposta de volta ao chat
+    const resp = match[1]; 
+    bott.sendMessage(chatId, resp); 
 });
 
-// Escuta qualquer tipo de mensagem
+
 bott.on('message', (msg) => {
    const chatId = msg.chat.id;
    bot.reply("local-user", msg.text).then(function(reply) {
