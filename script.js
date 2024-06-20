c6d5bc219008e30273a0a1802c8ab10084985f96
tpires1041 <tiagopires2501@outlook.com>
2024-06-20 09:09:37 -0400

    adicionado o json
    todas as 52 strings foram colocadas e são lidas em um arquivo .json

--- script.js
+++ script.js
@@ -1,17 +1,18 @@
 const axios = require('axios');
 const RiveScript = require('rivescript');
 const natural = require('natural');
+const fs = require('fs');
 
+let strings;
+fs.readFile('strings.json', 'utf8', (err, data) => {
+    if (err) {
+        console.error(err);
+        return;
+    }
+    strings = JSON.parse(data);
+});
 
-const string1 = "quem deve aguardar para doar sangue?"
-const string2 = "quais vacinas precisam aguardar?"
-const string3 = "quem não pode doar sangue?"
-const string4 = "quais são as recomendações para quem irá doar?"
-const string5 = "qual o intervalo entre doações?"
-const string6 = "o que preciso para doar sangue?"
-
-
-const brains = 'https://gist.githubusercontent.com/tpires1041/da6a583aeb2d840193b091b81ed77051/raw/fe6679bc3c084a8073ac5218b816a6874bbc2c8e/gistfile1.txt';
+const brains = 'https://gist.githubusercontent.com/tpires1041/da6a583aeb2d840193b091b81ed77051/raw/ec70d1b041daa946c84a064005e06fc40dab93af/gistfile1.txt';
 let bot = new RiveScript();
 
 axios.get(brains).then(response => {
@@ -22,11 +23,7 @@ axios.get(brains).then(response => {
 });
 
 const TelegramBot = require('node-telegram-bot-api');
-
-
 const token = '6972642226:AAEoBLk1WeoLB3mgyYA9QvEpFQG4XY89qzM';
-
-
 const bott = new TelegramBot(token, { polling: true });
 
 bott.onText((msg, match) => {
@@ -37,34 +34,23 @@ bott.onText((msg, match) => {
 
 bott.on('message', (msg) => {
     const chatId = msg.chat.id;
-
     let userInput = msg.text.toLowerCase();
 
-    if (natural.JaroWinklerDistance(userInput, string1) > 0.9 || userInput == 1){
-        userInput = 1;
-    } else {
-        if (natural.JaroWinklerDistance(userInput, string2) > 0.9 || userInput == 2){
-            userInput = 2;
-        } else {
-            if (natural.JaroWinklerDistance(userInput, string3) > 0.9 || userInput == 3){
-                userInput = 3;
-            } else {
-                if (natural.JaroWinklerDistance(userInput, string4) > 0.9 || userInput == 4){
-                    userInput = 4;
-                } else {
-                    if (natural.JaroWinklerDistance(userInput, string5) > 0.9 || userInput == 5){
-                        userInput = 5;
-                    } else {
-                        if (natural.JaroWinklerDistance(userInput, string6) > 0.9 || userInput == 6){
-                            userInput = 6;
-                        }
-                    }
-                }
-            }
-        }
+
+    const stringKeys = Object.keys(strings);
+
+    let matchedStringKey = stringKeys.find(key => 
+        natural.JaroWinklerDistance(userInput, strings[key]) > 0.9 || userInput == key.replace('string', '')
+    );
+
+    if (matchedStringKey) {
+        userInput = matchedStringKey.replace('string', '');
     }
 
     bot.reply("local-user", userInput).then(function (reply) {
+
         bott.sendMessage(chatId, reply);
+
+        bott.sendMessage(chatId, "Em que mais posso ajudar? 😁🤖");
     });
 });
